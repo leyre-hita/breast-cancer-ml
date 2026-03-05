479354752602a0563c0f324884695941baeccc59
leyre-hita <leyrehimar@gmail.com>
2026-03-05 21:29:31 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
+
+def prepare_data(path):
+
+    df = pd.read_csv(path)
+
+    X = df.drop('benign_0_mal_1', axis=1).values
+    y = df['benign_0_mal_1'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=101
+    )
+
+    scaler = MinMaxScaler()
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
