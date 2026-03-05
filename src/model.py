846d072ee223b4817cdb23502ddd5986807d226a
leyre-hita <leyrehimar@gmail.com>
2026-03-05 21:14:06 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,26 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
 
+
+def create_model():
+
+    model = Sequential()
+
+    # Primera capa oculta
+    model.add(Dense(units=30, activation='relu'))
+    model.add(Dropout(0.5))
+
+    # Segunda capa oculta
+    model.add(Dense(units=15, activation='relu'))
+    model.add(Dropout(0.5))
+
+    # Capa de salida (clasificación binaria)
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    # Compilación
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy'
+    )
+
+    return model
